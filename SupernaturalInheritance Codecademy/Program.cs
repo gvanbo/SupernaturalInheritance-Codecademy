1224b3cb385dc81157a83ff61d9c75f2a290874f
gvanbo <gvanbo@gmail.com>
2021-05-02 19:00:59 -0600

    Add project files.

---
+++ SupernaturalInheritance Codecademy/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SupernaturalInheritance_Codecademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Storm s1 = new Storm("Sand Storm", true, "Gandalf the Grey");
+
+            Console.WriteLine(s1.Announce());
+        }
+    }
+}
---
+++ SupernaturalInheritance Codecademy/Storm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupernaturalInheritance_Codecademy
+{
+    class Storm
+    {
+        public string Essence { get; protected set; }
+        public bool IsString { get; private set; }
+        public string Caster { get; protected set; }
+
+        /// <summary>
+        /// Define the storm with the type of the storm, boolean if storm is strong and name of Caster
+        /// </summary>
+        /// <param Essence="essence"></param>
+        /// <param IsString="isString"></param>
+        /// <param Caster="caster"></param>
+        public Storm (string essence, bool isString, string caster)
+        {
+            Essence = essence;
+            IsString = isString;
+            Caster = caster;
+        }
+
+        /// <summary>
+        /// Announce the Storm summary.
+        /// </summary>
+        /// <returns>message</returns>
+        public string Announce()
+        {
+            string strength;
+            if (IsString == true) strength = "strong";
+            else strength = "weak";
+            string message = $"{Caster} casts a {strength} {Essence}.";
+            return message;
+        }
+
+    }
+}
